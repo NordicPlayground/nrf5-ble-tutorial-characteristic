e769b7fefdfcce63464a370874286c1adf7684c5
Romero <Marjeris.Romero@nordicsemi.no>
2018-09-06 14:04:54 +0200

    Challenge 1: alter our_temperature_characteristic_update() to only send notifications when the temperature has changed

--- main.c
+++ main.c
@@ -150,9 +150,21 @@ void assert_nrf_callback(uint16_t line_num, const uint8_t * p_file_name)
 static void timer_timeout_handler(void * p_context)
 {
     // OUR_JOB: Step 3.F, Update temperature and characteristic value.
-		int32_t temperature = 0;
-		sd_temp_get(&temperature);
-		our_termperature_characteristic_update(&m_our_service, &temperature);
+		int32_t temperature = 0;      //Declare a variable holding temperature value
+                static int32_t previous_temperature=0;      //Declare a variable to store current temperature until next measurement. 
+
+		sd_temp_get(&temperature);    //Get temperature
+                
+
+                // Check if current temperature is different from last temperature
+                if(temperature != previous_temperature)
+                    {
+                      // If new temperature then send notification
+                      our_termperature_characteristic_update(&m_our_service, &temperature);
+                    }
+                
+                // Save current temperature until next measurement
+                previous_temperature = temperature;
 		nrf_gpio_pin_toggle(LED_4);
 }
 
